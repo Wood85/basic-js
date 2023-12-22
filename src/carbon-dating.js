8a0c9804f392fa87944224a48c050cd4a6ec49ba
Aleksandr <megalistnn@gmail.com>
2023-12-22 17:12:03 +0300

    feat: implement function isMAC48Address

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -30,10 +30,6 @@ function dateSample(sampleActivity) {
 	}
 	return false;
 }
-// function dateSample(/* sampleActivity */) {
-//   throw new NotImplementedError('Not implemented');
-//   // remove line with error and write your code here
-// }
 
 module.exports = {
 	dateSample
--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+	const adressArr = n.split("-");
+	if (adressArr.length !== 6) {
+		return false;
+	}
+	for (let i = 0; i < adressArr.length; i++) {
+		if (/^[0-9A-Fa-f]{2}$/.test(adressArr[i]) === false) {
+			return false;
+		}
+	}
+	return true;
 }
+
 module.exports = {
-  isMAC48Address
+	isMAC48Address
 };
