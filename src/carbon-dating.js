51ac9a6bda858dd1ed465a67a2c0f7c56ca1ecd0
Aleksandr <megalistnn@gmail.com>
2023-12-22 16:25:05 +0300

    feat:implement function dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,24 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+	if (typeof sampleActivity !== "string") {
+		return false;
+	}
+	let activity = +sampleActivity;
+	if (activity > 0 && activity < MODERN_ACTIVITY) {
+		let age = Math.ceil(
+			(Math.log(MODERN_ACTIVITY / activity) * HALF_LIFE_PERIOD) / 0.693
+		);
+		return age;
+	}
+	return false;
 }
+// function dateSample(/* sampleActivity */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// }
 
 module.exports = {
-  dateSample
+	dateSample
 };
